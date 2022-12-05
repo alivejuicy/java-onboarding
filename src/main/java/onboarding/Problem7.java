43bfd36ed8192aedc394a0a497d5adddf988cd59
alivejuicy <kkddhh1998@gmail.com>
2022-12-05 18:03:00 +0900

    feat(isFriendsContainUser) : List<List<String>> 형태의 friend 리스트에 user가 포함되어 있는지 검증하는 메소드

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,22 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
-        return answer;
+        //ist<String> answer = Collections.emptyList();
+        //return answer;
+        return visitors;
+    }
+
+    /**
+     * @param friend
+     * <p>친구 리스트 목록</p>
+     * @param user
+     * <p>추천받는 대상인 유저</p>
+     * @return 친구 목록 리스트에서 user 유무에 따라 boolean 값을 리턴한다.
+     */
+    private static boolean isFriendsContainUser(List<String> friend, String user) {
+        return friend.contains(user);
     }
 }
